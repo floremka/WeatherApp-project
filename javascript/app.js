282a0ea48a12e164b1dd9771e2e5b0d711b674fb
floremka <114143942+floremka@users.noreply.github.com>
2024-03-15 00:17:28 +0200

    add javascript

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,38 @@
-function search() {}
+function inputCity(event) {
+  event.preventDefault();
+
+  let inputText = document.querySelector("#input-text");
+
+  let h1 = document.querySelector("#h1Text");
+  if (inputText.value) {
+    h1.innerHTML = inputText.value;
+  } else {
+    h1.innerHTML = null;
+    alert("sorry enter city");
+  }
+}
+
+let form = document.querySelector("#firstForm");
+form.addEventListener("submit", inputCity);
+
+function timeCity() {
+  let currentTime = new Date();
+
+  let Hours = currentTime.getHours();
+  let minute = currentTime.getMinutes();
+  let second = currentTime.getSeconds();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentTime.getDay()];
+  let times = document.querySelector("#time");
+  times.innerHTML = `${day} ${Hours}: ${minute} : ${second}`;
+}
+timeCity();
