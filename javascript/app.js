e2fc5af776569bb08cb6a17337bcc2e43ee5bf96
floremka <114143942+floremka@users.noreply.github.com>
2024-04-17 06:38:28 +0200

    inject forecast weather

--- javascript/app.js
+++ javascript/app.js
@@ -1,24 +1,25 @@
 function dispalayTemparature(response) {
-  console.log(response.data.temparature.current);
-  let temparature = document.querySelector("#temp");
-  let currentTemparature = Math.round(response.data.temparature.current);
-  temparature.innerHTML = `${currentTemparature}`;
+  console.log(response.data.temperature.current);
+  let temperature = document.querySelector("#temp");
+  let currentTemperature = Math.round(response.data.temperature.current);
+  temperature.innerHTML = `${currentTemperature}`;
 }
-function inputCity(event, response) {
+function inputCity(event) {
   event.preventDefault();
-  console.log(response.data.temeparature.current);
-  city = inputText.value;
-  key = ea956222f2o8bbfat9d06fcb925a34cb;
+
   let inputText = document.querySelector("#input-text");
 
-  let h1 = document.querySelector("#h1Text");
+  let h1 = document.querySelector("#cityElement");
   if (inputText.value) {
     h1.innerHTML = inputText.value;
   } else {
     h1.innerHTML = null;
     alert("sorry enter city");
   }
-  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}`;
+
+  let city = inputText.value;
+  let apiKey = "ea956222f2o8bbfat9d06fcb925a34cb";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiURL).then(dispalayTemparature);
 }
 
@@ -45,4 +46,34 @@ function timeCity() {
   let times = document.querySelector("#time");
   times.innerHTML = `${day} ${Hours}: ${minute} : ${second}`;
 }
+
+function displayFocast() {
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  let forcastHTML = "";
+
+  days.forEach(function (day) {
+    forcastHTML =
+      forcastHTML +
+      `
+<div class="weatherDay">
+<div class ="forecastDay">${day}</div>
+<div class= "forecastIcon">☀</div>
+<div class="forecastTemperatures">
+<div class ="forecastTemperature">
+<strong>15&#176;</strong>
+</div>
+<div class="forecastTemparature">9&#176;</div>
+
+</div>
+
+</div>
+
+
+`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forcastHTML;
+}
+
 timeCity();
+displayFocast();
